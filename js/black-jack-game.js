a6fb9a00cc2e6c6b22af3d753278472ef98af987
Matt Ryan <matthewtryan99@gmail.com>
2020-02-10 10:58:56 -0600

    hit button

--- js/black-jack-game.js
+++ js/black-jack-game.js
@@ -3,6 +3,30 @@
 let buttons = document.querySelector('.buttons');
 let messageBarPlay = document.querySelector('#player');
 let messageBarDeal = document.querySelector('#dealer');
+let body = document.body;
+let dealCount = 0;
+let scorePlay = 0;
+let scoreDeal = 0;
+let cardCount = 0;
+
+let scoreBoardPlayer = document.querySelector("#player-points");
+let scoreBoardDealer = document.querySelector("#dealer-points");
+
+
+// function createAlert(title, para)
+// {
+//     let alert = document.createElement('div');
+//     alert.innerHTML = '<div class="modal" tabindex="-1" role="dialog">';
+//     alert.innerHTML += '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">';
+//     alert.innerHTML += title;
+//     alert.innerHTML += '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><p>';
+//     alert.innerHTML += para;
+//     alert.innerHTML += '</p></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>';
+
+//     return alert;
+// }
+
+
 
 function createImage(imageURl, classes){
     var newImage = document.createElement('img')
@@ -15,34 +39,51 @@ function createImage(imageURl, classes){
 // console.log(buttons);
 
 buttons.addEventListener('click', function(e){
-    if (e.target.textContent == 'Deal')
+    if (e.target.textContent == 'Deal' && dealCount == 0)
     {
-        for(let i = 0; i < 4; i++)
-        {
-            if(i < 2){
-                let newImage = createImage(cards[i].image, "image");
+        for(cardCount; cardCount < 4; cardCount++){
+            if(cardCount < 2){
+                let newImage = createImage(cards[cardCount].image, "image");
                 messageBarPlay.append(newImage);
+                scorePlay += cards[cardCount].value;
+
             }
-            else if (i == 2)
+            else if (cardCount == 2)
             {
-                let newImage = createImage(cards[i].image, "image");
+                let newImage = createImage(cards[cardCount].image, "image");
                 messageBarDeal.append(newImage);
+                scoreDeal += cards[cardCount].value;
             }
             else
             {
-                let newImage = createImage("images/blue_back.jpg", "image");
-                messageBarDeal.append(newImage);
+                let backImage = createImage("images/blue_back.jpg", "image back-image");
+                messageBarDeal.append(backImage);
             }
         }
+        dealCount = 1;
     }
     if (e.target.textContent == 'Hit')
     {
-        console.log("Hit is working");
+        let newImage = createImage(cards[cardCount].image, "image");
+        messageBarPlay.append(newImage);
+        scorePlay += cards[cardCount].value;
+        cardCount++;
+
+        if(scorePlay > 21)
+        {
+            // let alert = createAlert("BUST", "You went over 21.");
+            // body.append(alert);
+        }
     }
     if (e.target.textContent == 'Stand')
     {
-        console.log("Stand is working");
+        let newBack = document.querySelector(".back-image");
+        newBack.setAttribute('src', card[3].image);
+        scoreDeal += cards[3].value;
     }
+
+    scoreBoardDealer.textContent = scoreDeal;
+    scoreBoardPlayer.textContent = scorePlay;
 })
 
 
